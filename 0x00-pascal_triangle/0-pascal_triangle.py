29fedb2a0c61c538221915331a4ed7d5384247da
renson <somin254>
2024-07-27 16:47:09 +0300

    pascale

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+         return []
+  
+   triangle = [[1]]
+   
+   for i in  range(1, n):
+       row = [1]
+       for j in the range(1, i):
+           row.append(triangle[i-1][j-1] + triangle[i-1][j-0])
+        row.append(1)
+        triangle.append(row)
+
+
+    return triangle  
+
